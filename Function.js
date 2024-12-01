71a35d49893667c037ada97cf6a0a16498c9f0af
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-12-01 15:19:27 +0530

    basics of js

---
+++ Function.js
@@ -0,0 +1,30 @@
+// Name funtion
+// function func(a) {
+// console.log(a);
+// }
+
+// let a = 5;
+// func(a);
+
+// Named function with return type:
+
+// function sum(a,b) { 
+//     return a + b;
+// }
+
+// let a = 4;
+// let b = 3;
+// let c = sum(a,b);
+// console.log(c);
+
+let sum =(p1,p2)=> {
+    console.log("Hello World");
+    return p1+p2;
+};
+let a = 4;
+let b = 5;
+let c = sum(a,b);
+console.log(c);
+
+
+
---
+++ script.js
@@ -0,0 +1,14 @@
+// let const var
+
+// var a = "hello";
+// console.log(a)
+// var a = "world"
+// console.log(a)
+
+// let a = "hello";
+// console.log(a);
+// a = "world";
+// console.log(a);
+
+const a = "Hello";
+console.log(a);
