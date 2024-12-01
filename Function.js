f2cc1ff126a5e39ec6454b9d82ce7983c7c21870
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-12-01 15:44:54 +0530

    Basics revesion of js

--- Function.js
+++ Function.js
@@ -17,14 +17,36 @@
 // let c = sum(a,b);
 // console.log(c);
 
-let sum =(p1,p2)=> {
-    console.log("Hello World");
-    return p1+p2;
-};
-let a = 4;
-let b = 5;
-let c = sum(a,b);
-console.log(c);
+// let sum =(p1,p2)=> {
+//     console.log("Hello World");
+//     return p1+p2;
+// };
+// let a = 4;
+// let b = 5;
+// let c = sum(a,b);
+// console.log(c);
 
+// let c = [1,2,3,4,5,6];
+// console.log(c);
 
+// let d = [1,2,'shubham',4,5,6];
+// console.log(d);
 
+// Methods
+// Map
+// Slice
+// Splice
+// forEach
+// for of
+
+//How to use map method
+// let e = d.map((x)=> {
+//     return x * x;
+// });
+// console.log(e);
+
+// let data = {
+//     a: 5,
+//     b: 34,
+//     c: 556
+// }
